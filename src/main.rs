3399807abaffb7d1702ff9ff2219eb75879f3635
Ech0n <matid@spoko.pl>
2023-04-04 10:40:25 +0200

    TODO: Fix cors

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@ async fn hello() -> impl Responder {
 }
 
 #[post("/word")]
-async fn echo(req_body: String) -> impl Responder {
+async fn word(req_body: String) -> impl Responder {
+    println!("aaaaa");
     HttpResponse::Ok().body(req_body)
 }
 
@@ -18,20 +19,11 @@ async fn manual_hello() -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        let cors = Cors::default()
-              .allowed_origin("https://www.rust-lang.org")
-              .allowed_origin_fn(|origin, _req_head| {
-                  origin.as_bytes().ends_with(b".rust-lang.org")
-              }).allowed_methods(vec!["GET", "POST"])
-              .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
-              .allowed_header(http::header::CONTENT_TYPE)
-              .max_age(3600);
+        let cors = Cors::permissive();
             
         App::new()
             .wrap(cors)
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .service(word)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
