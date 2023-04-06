245f721ce6c0879a22bf45e60a0792fd0c412813
Ech0n <43513980+Ech0n@users.noreply.github.com>
2023-04-06 17:49:12 +0200

    Added server side word recognition, (Only for main word, words that are created as a side effect are ignored)

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use actix_cors::Cors;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, Error};
 use std::sync::Mutex;
-use serde_json::{Value};
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize};
+use std::io::Read;
+use std::io::BufReader;
+use std::fs::File;
+
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -13,32 +16,89 @@ struct BoardState{
     cells:Mutex<[[u8;15];15]>,
 }
 
+//Todo : implement DAWG structure as its far more efective than simple word list
+struct WordsDict{
+    words:Vec<String>,
+}
+
 #[derive(Deserialize,Debug)]
 struct Move{
     letters: [u8;15],
     column: bool,
-    num:u8,
+    num: usize,
 }
+
 
 #[post("/word")]
 async fn word(req_body: String , data: web::Data<BoardState> ) -> Result<HttpResponse, Error> {
 
     let v: Move = serde_json::from_str(&req_body)?;
     let mut cells = data.cells.lock().unwrap();
-    for i in 0..15{
-        if v.letters[i]!=0
-        {
-            if v.column {
-                cells[i][v.num as usize] = v.letters[i];
-            }
-        }
+    let mut words_to_check : Vec<String> = Vec::new();
+    let mut temp : String = String::new();
+    let mut i = 0;
+    let mut col:usize = 0;
+    let mut row:usize = 0;
+
+    // Sprawdzanie wyrazu cells[num*column as u8+(i * !column as u8)][i*column as u8+(num * !column as u8)]
+    while v.letters[i] == 0 && i<15
+    {
+        i +=1;
     }
-    println!("{:?}",cells);
+    //Doszlismy do poczatku wyrazu 
+    // Teraz sie cofamy aby sprawdzic czy przed wyrazem znajduja sie juz jakies litery
+    let mut j = i-1;
+    col = v.num*((v.column )as usize)+(j * !v.column as usize);
+    row = j*v.column as usize+(v.num * (!v.column) as usize);
+    while j>0 &&  cells[col][row] != 0
+    {
+        println!("Cofamy sie!");
+        temp.insert_str(0, &(cells[col][row] as char).to_string() );
+        j-=1;
+        col = v.num*((v.column )as usize)+(j * !v.column as usize);
+        row = j*v.column as usize+(v.num * (!v.column) as usize);
+    }
+    col = v.num*((v.column )as usize)+(i * !v.column as usize);
+    row = i*v.column as usize+(v.num * (!v.column) as usize);
+    while i<15 && (v.letters[i] | cells[col][row])!= 0
+    {
+        temp.push((v.letters[i] | cells[col][row])  as char);
+        cells[col][row] |= v.letters[i]; 
+        i+=1;
+        col = v.num*((v.column )as usize)+(i * !v.column as usize);
+        row = i*v.column as usize+(v.num * (!v.column) as usize);
+    }
+    
+    println!("Nowe slowo: {}",temp);
+    // println!("{:?}",cells);
     Ok(HttpResponse::Ok().body("Halo"))
 }
 
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    //open file containing words
+    // In production its best to load a test file because a larga file might take a while to load
+    let f = File::open("A:\\Documents\\Projekty\\letteraki\\src\\production.txt")?;
+    let mut reader = BufReader::new(f);
+    let mut buffer: Vec<u8> = Vec::new();
+    let mut words : Vec<String> = Vec::new();
+    reader.read_to_end(&mut buffer)?;
+    let mut tempstr : String = String::new();
+    println!("Loading words!");
+    for value in buffer {
+        if value as char == '\n'{
+            words.push(tempstr);
+            tempstr = String::new();
+        }else
+        {
+            tempstr.push( value as char);
+        }
+    }
+    println!("Finished loading words!");
+    let words = web::Data::new(WordsDict{
+        words: words,
+    });
     let board = web::Data::new( BoardState {
         cells: Mutex::new([[0;15];15]),
     });
@@ -47,6 +107,7 @@ async fn main() -> std::io::Result<()> {
         let cors = Cors::permissive();
         App::new()
             .app_data(board.clone())
+            .app_data(words.clone())
             .wrap(cors)
             .service(word)
     })
